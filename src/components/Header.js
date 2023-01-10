0808c18e6f0da11e261044006784461df8437943
Manir Uddin <manir22sust@gmail.com>
2023-01-10 10:37:03 +0100

    Implemented functionality add to cart,remove to cart, updated cart with price, sorting, filtering, search

---
+++ src/components/Header.js
@@ -0,0 +1,87 @@
+import Dropdown from "react-bootstrap/Dropdown";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { useCart } from "../hooks/useCart";
+
+export const Header = () => {
+  const {
+    state: { cart },
+    dispatch,
+    productDispatch,
+  } = useCart();
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container fluid>
+        <Navbar.Brand>
+          <Link to="/">Shopping Cart</Link>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <Form.Control
+            style={{ width: 450 }}
+            placeholder="Search"
+            className="me-auto"
+            aria-label="Search"
+            onChange={(e) => {
+              productDispatch({
+                type: "FILTER_BY_SEARCH",
+                payload: e.target.value,
+              });
+            }}
+          />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown align={{ lg: "right" }}>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart />
+              <Badge> {cart.length}</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((prod) => (
+                    <span className="cartitem" key={prod.id}>
+                      <img
+                        src={prod.image}
+                        className="cartItemImg"
+                        alt={prod.name}
+                      />
+                      <div className="cartItemDetail">
+                        <span>{prod.name}</span>
+                        <span>€ {prod.price.split(".")[0]}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                          })
+                        }
+                      />
+                    </span>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go To Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{ padding: 10 }}>Cart is Empty</span>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
---
+++ src/page/Home.js
@@ -0,0 +1,52 @@
+import { Filter } from "../components/Filter";
+import { SingleProduct } from "../components/SingleProduct";
+import { useCart } from "../hooks/useCart";
+
+import "./styles.css";
+
+export const Home = () => {
+  const {
+    state: { products },
+    productState: { sort, byStock, byFastDelivery, byRating, searchQuery },
+  } = useCart();
+
+  const transformProducts = () => {
+    let sortedProducts = products;
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock);
+    }
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.fastDelivery);
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter(
+        (prod) => prod.ratings >= byRating
+      );
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.name.toLowerCase().includes(searchQuery)
+      );
+    }
+    return sortedProducts;
+  };
+
+  return (
+    <div className="home">
+      <Filter />
+
+      <div className="productContainer">
+        {transformProducts().map((product) => {
+          return <SingleProduct product={product} key={product.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
